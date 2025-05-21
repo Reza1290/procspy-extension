f62f8fe5f9a11feb61b07c23dc234e9213d0c3ec
Reza 1290 <reza.muktasib@gmail.com>
2025-05-21 19:50:51 +0700

    fix: chrome injection

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -52,7 +52,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     });
   }
 
-  if (changeInfo.status === 'complete') {
+  if (changeInfo.status === 'complete' && !tab.url?.startsWith("chrome://")) {
     chrome.scripting
       .executeScript({
         target: { tabId: tabId },
