2ca5fa4ab2a73a618886766535939930fec996cc
Reza 1290 <reza.muktasib@gmail.com>
2025-06-11 07:38:54 +0700

    fix: permission file

---
+++ src/scripts/ask_permissions.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        const cam = stream.getVideoTracks()[0]?.getSettings();
+        const mic = stream.getAudioTracks()[0]?.getSettings();
+
+        console.log("Devices:", { cam, mic });
+
+        stream.getTracks().forEach(track => track.stop());
+
+        window.close()
+    } catch (err) {
+        console.error("Media error:", err);
+        window.alert('Please Approve Device Permission for The Extension (go to settings or Reinstall)')
+        window.location.href = `chrome://settings/content#media-stream-mic`
+    } finally {
+        // window.close()
+    }
+});
